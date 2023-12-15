72b2938809ca5c8249cf4e6778f8dd8f02d555ed
Igor <ceo@primecore.io>
2023-12-15 16:57:12 +0100

    added link

--- main.go
+++ main.go
@@ -10,75 +10,98 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 )
 
-// Структура для получения текста от пользователя
+// UserInput структура для получения текста от пользователя
 type UserInput struct {
 	Text string `json:"text"`
 }
 
-// Функция для включения CORS
+// enableCors функция для включения CORS
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-// Функция для подключения к базе данных
+// dbConn функция для подключения к базе данных
 func dbConn() (db *sql.DB) {
-
 	dbDriver := "mysql"
 	dbUser := "root"     // Замените на своего пользователя
 	dbPass := "12345678" // Ваш пароль
 	dbName := "mydb"     // Имя вашей базы данных
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp(127.0.0.1)/"+dbName+"?parseTime=true")
-
 	if err != nil {
 		panic(err.Error())
 	}
 	return db
 }
 
-// Функция для сохранения зашифрованного текста
+// saveText функция для сохранения зашифрованного текста
 func saveText(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w) // Включение CORS для этого обработчика
-
-	log.Println("Received a request to save text") // Логирование приходящего запроса
+	enableCors(&w)
 
 	if r.Method == "OPTIONS" {
-		return // Обработка предварительных запросов CORS
+		return
 	}
 
 	if r.Method == "POST" {
 		var userText UserInput
 		err := json.NewDecoder(r.Body).Decode(&userText)
 		if err != nil {
-			log.Printf("Error decoding request: %s", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		hashedText := sha256.Sum256([]byte(userText.Text))
 		db := dbConn()
-		insert, err := db.Prepare("INSERT INTO texts(encrypted_text) VALUES(?)")
+		newUUID := uuid.New().String()
+		insert, err := db.Prepare("INSERT INTO texts(uuid, encrypted_text) VALUES(?, ?)")
 		if err != nil {
-			log.Printf("Error preparing database query: %s", err)
 			panic(err.Error())
 		}
-		_, err = insert.Exec(hex.EncodeToString(hashedText[:]))
+		_, err = insert.Exec(newUUID, hex.EncodeToString(hashedText[:]))
 		if err != nil {
-			log.Printf("Error executing database query: %s", err)
 			panic(err.Error())
 		}
 
-		fmt.Fprintf(w, "Text saved successfully")
+		response := map[string]string{"uuid": newUUID}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+
 		defer db.Close()
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
 
+// getText функция для получения и отображения текста по UUID
+func getText(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	uuid := r.URL.Path[len("/text/"):]
+
+	db := dbConn()
+	query, err := db.Prepare("SELECT encrypted_text FROM texts WHERE uuid = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer query.Close()
+
+	var encryptedText string
+	err = query.QueryRow(uuid).Scan(&encryptedText)
+	if err != nil {
+		http.Error(w, "Text not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, encryptedText)
+	defer db.Close()
+}
+
 func main() {
 	http.HandleFunc("/save", saveText)
+	http.HandleFunc("/text/", getText)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <input v-model="userText" placeholder="Введите текст">
     <button @click="saveText">Save</button>
+    <!-- Элемент для отображения ссылки -->
+    <div v-if="link">
+      Поделитесь этой ссылкой: <a :href="link" target="_blank">{{ link }}</a>
+    </div>
   </div>
 </template>
 
@@ -9,8 +13,9 @@
 export default {
   data() {
     return {
-      userText: ''
-    }
+      userText: '',
+      link: '' // Ссылка, которая будет отображаться после сохранения текста
+    };
   },
   methods: {
     async saveText() {
@@ -27,9 +32,11 @@ export default {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const data = await response.text();
+        const data = await response.json(); // Предполагается, что сервер возвращает JSON объект с полем uuid
+        this.link = `http://localhost:8080/text/${data.uuid}`; // Формирование ссылки
+
         console.log('Success:', data);
-        alert('Text saved');
+        alert('Text saved successfully');
       } catch (error) {
         console.error('Error:', error);
         alert('Failed to save text');
@@ -38,3 +45,7 @@ export default {
   }
 }
 </script>
+
+<style>
+/* Ваши стили */
+</style>
